561baa0a6c3ae61b62cf10d4a6573a875d844cd5
VyD1nk1902 <vy.dinh.2929@gmail.com>
2025-03-13 12:43:10 +0700

    Add function dropdown to warning__list in warning section.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,35 @@
 const warningHeader = document.querySelectorAll(".warning__header");
+// tạo biến trỏ tới .warning__header
 
-warningHeader.forEach((item) => item.addEventListener("click", handleShowDropdown));
-
-function handleShowDropdown(e) {
-  console.log(e.target.parentNode);
-}
+warningHeader.forEach((item) => item.addEventListener("click", handleShowDropdown)); //truyền thao tác click -> function dropdown
 
 function handleShowDropdown(e) {
   // const warningItem = e.target.parentNode;*
   // warningItem.classList.toggle("active"); *
+  const warningContent = e.target.nextElementSibling; //biến trỏ tới .warning__content
+  const warningContentAll = document.querySelectorAll(".warning__content");
+  const warningIcon = e.target.querySelector(".warning__header-icon");
+  const warningIconAll = document.querySelectorAll(".warning__header-icon");
+
+  warningIconAll.forEach((item) => {
+    if (item !== warningIcon) {
+      item.classList.remove("active");
+    }
+  });
+
+  warningContentAll.forEach((item) => {
+    if (item != warningContent) {
+      item.style.height = 0;
+      item.classList.remove("active");
+    }
+    // nếu item ko p là content đang được chọn thì height sẽ = 0 và rm active
+  }); // các warning__content sẽ tự động inactive khi click tới cái khác
+
+  warningContent.style.height = `${warningContent.scrollHeight}px`;
+  warningContent.classList.toggle("active");
+  warningIcon.classList.toggle("active");
+
+  if (!warningContent.classList.contains("active")) {
+    warningContent.style.height = 0;
+  } //nếu không phải warningContent được click(active) thì ẩn
 }
