4ab12299ff848440d0ece41fc8544e6b4e9b9650
VyD1nk1902 <vy.dinh.2929@gmail.com>
2025-03-19 16:39:29 +0700

    Add a fixed header when scrolling down page
    Add an animation for smoother of fixed header,
    use JS to make the fixed header appear when user
    scrolling down.

---
+++ assets/js/app.js
@@ -0,0 +1,15 @@
+// Header fixed
+const header = document.querySelector(".header");
+
+window.addEventListener("scroll", () => {
+  const headerHeight = header.offsetHeight;
+  const marginTopHeader = parseFloat(getComputedStyle(header).marginTop);
+  const totalHeaderHeight = headerHeight + marginTopHeader;
+  if (window.scrollY > totalHeaderHeight) {
+    header.classList.add("fixed");
+    document.body.style.paddingTop = `${totalHeaderHeight}`;
+  } else {
+    header.classList.remove("fixed");
+    document.body.style.paddingTop = 0;
+  }
+});
