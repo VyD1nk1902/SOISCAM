265a91c9709d38ab72aea023649d8892106448bc
VyD1nk1902 <vy.dinh.2929@gmail.com>
2025-03-24 14:20:19 +0700

    Done optimally for report

--- assets/js/report.js
+++ assets/js/report.js
@@ -7,7 +7,16 @@ let arrayImage = [];
 const phoneScammerInput = document.getElementById("phoneScammer");
 const bankNumberInput = document.getElementById("bankNumber");
 const phoneSenderInput = document.getElementById("phoneSender");
+const contentReportInput = document.getElementById("contentReport");
 // end VARIABLES
+
+//HANDLE CONTENT REPORT INPUT
+contentReportInput.addEventListener("input", (e) => {
+  e.target.style.height = "auto";
+  e.target.style.height = `${e.target.scrollHeight}px`;
+});
+
+//end HANDLE CONTENT REPORT INPUT
 
 //HANDLE INPUT ONLY NUMBER
 phoneScammerInput.addEventListener("keypress", handleInputOnlyNumber);
@@ -78,36 +87,37 @@ Validator({
   formGroupSelector: ".form__group",
   errorSelector: ".form-message",
   rules: [
-    Validator.isRequired("#nameScammer", "Vui lòng nhập tên tài khoản đối tượng"),
-    Validator.minLength("#nameScammer", 6, "Nhập tối thiểu 6 ký tự"),
-    Validator.isRequired("#phoneScammer", "Nhập số điện thoại của đối tượng"),
-    Validator.isPhoneNumber("#phoneScammer"),
-    Validator.isRequired("#bankNumber", "Nhập số tài khoản đối tượng"),
-    Validator.isNumber("#bankNumber", "Vui lòng nhập số"),
-    Validator.minLength("#bankNumber", 8, "Nhập tối thiểu 8 ký tự"),
-    Validator.isRequired("#bankName", "Nhập tên ngân hàng"),
-    Validator.minLength("#bankName", 3, "Nhập tối thiểu 3 ký tự"),
-    Validator.isRequired("#contentReport", "Nhập nội dung tố cáo"),
-    Validator.minLength("#contentReport", 20, "Nhập tối thiểu 20 ký tự"),
-    Validator.isImageUploaded("#uploadImages"),
-    Validator.isRequired("#nameSender", "Vui lòng nhập tên người tố cáo"),
-    Validator.minLength("#nameSender", 6, "Nhập tối thiểu 6 ký tự"),
-    Validator.isRequired("#phoneSender", "Vui lòng nhập số điện thoại của người tố cáo"),
-    Validator.isPhoneNumber("#phoneSender"),
+    // Validator.isRequired("#nameScammer", "Vui lòng nhập tên tài khoản đối tượng"),
+    // Validator.minLength("#nameScammer", 6, "Nhập tối thiểu 6 ký tự"),
+    // Validator.isRequired("#phoneScammer", "Nhập số điện thoại của đối tượng"),
+    // Validator.isPhoneNumber("#phoneScammer"),
+    // Validator.isRequired("#bankNumber", "Nhập số tài khoản đối tượng"),
+    // Validator.isNumber("#bankNumber", "Vui lòng nhập số"),
+    // Validator.minLength("#bankNumber", 8, "Nhập tối thiểu 8 ký tự"),
+    // Validator.isRequired("#bankName", "Nhập tên ngân hàng"),
+    // Validator.minLength("#bankName", 3, "Nhập tối thiểu 3 ký tự"),
+    // Validator.isRequired("#contentReport", "Nhập nội dung tố cáo"),
+    // Validator.minLength("#contentReport", 20, "Nhập tối thiểu 20 ký tự"),
+    // Validator.isImageUploaded("#uploadImages"),
+    // Validator.isRequired("#nameSender", "Vui lòng nhập tên người tố cáo"),
+    // Validator.minLength("#nameSender", 6, "Nhập tối thiểu 6 ký tự"),
+    // Validator.isRequired("#phoneSender", "Vui lòng nhập số điện thoại của người tố cáo"),
+    // Validator.isPhoneNumber("#phoneSender"),
   ],
   onSubmit: async function ({ images, ...rest }) {
     console.log({ images: arrayImage, ...rest }); // Dữ liệu form sau khi xác thực thành công
+    const formImagePreviews = document.querySelectorAll(".form__image-preview");
     try {
       axios.post(endpoint, {
         images: arrayImage,
         ...rest,
       });
-      const formImagePreviews = document.querySelectorAll(".form__image-preview");
-      formImagePreviews.forEach((item) => item.remove());
       FuiToast.success("Gửi đơn thành công.");
+      formImagePreviews.forEach((item) => item.remove());
     } catch (error) {
       console.log(error);
       FuiToast.error("Gửi đơn thất bại.");
+      formImagePreviews.forEach((item) => item.remove());
     }
   },
   resetOnSubmit: true,
