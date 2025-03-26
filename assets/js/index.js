a6b960bd193593e5adb11abd40a117c70a2e4138
VyD1nk1902 <vy.dinh.2929@gmail.com>
2025-03-26 20:26:06 +0700

    Move modalHTML into handleShowModal function and adjust showmodal to have the same id as data that been clicked.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,99 +1,12 @@
-// MODAL DETAIL SCAMMER
-const ModalHTML = `
- <section class="modal">
-        <div class="modal__overlay"></div>
-        <div class="modal__content">
-          <div class="modal__header">
-            <div class="modal__header-title">Chi tiết tố cáo</div>
-            <div class="modal__header-close">
-              <img src="./assets/images/icon/close-icon.svg" alt="close icon" />
-            </div>
-          </div>
-          <div class="modal__body">
-            <div class="modal__group">
-              <div class="modal__profile">
-                <div class="modal__profile-avatar">
-                  <img src="./assets/images/Avatars/avatar-1.png" alt="avatar-scammer" />
-                </div>
-                <div class="modal__info">
-                  <h4 class="modal__info-name">Nguyễn Văn A</h4>
-                  <p class="modal__info-dsc">#50 - Tố vào ngày 12/02/2024</p>
-                </div>
-              </div>
-              <div class="modal__detail">
-                <span class="modal__detail-title">Số điện thoại</span>
-                <span class="modal__detail-text">0333990859</span>
-              </div>
-              <div class="modal__detail">
-                <span class="modal__detail-title">Số tài khoản</span>
-                <span class="modal__detail-text">342423443</span>
-              </div>
-              <div class="modal__detail">
-                <span class="modal__detail-title">Ngân hàng</span>
-                <span class="modal__detail-text">ACB</span>
-              </div>
-            </div>
-            <div class="modal__group">
-              <div class="modal__profile">
-                <div class="modal__profile-avatar">
-                  <img src="./assets/images/Avatars/avatar-2.png" alt="avatar-scammer" />
-                </div>
-                <div class="modal__info">
-                  <h4 class="modal__info-name">Nguyễn Văn B</h4>
-                  <p class="modal__info-dsc">Người tố cáo</p>
-                </div>
-              </div>
-              <div class="modal__detail">
-                <span class="modal__detail-title">Trạng thái</span>
-                <span class="modal__detail-text">Nạn nhân</span>
-              </div>
-              <div class="modal__detail">
-                <span class="modal__detail-title">Liên hệ</span>
-                <span class="modal__detail-text">0356655665</span>
-              </div>
-              <div class="modal__textarea">
-                <span class="modal__detail-title">Nội dung tố cáo</span>
-                <p class="modal__textarea-content">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente asperiores,
-                  tenetur eius a, nemo incidunt necessitatibus sunt vitae voluptates molestias
-                  aliquid explicabo dolore quia possimus fugiat eveniet veniam inventore nisi.
-                </p>
-              </div>
-              <div class="modal__images">
-                <span class="modal__detail-title">Hình ảnh liên quan</span>
-                <div class="modal__preview-images">
-                  <img
-                    src="https://images.unsplash.com/photo-1726066012751-2adfb5485977?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                    alt="image scammer"
-                  />
-                  <img
-                    src="https://images.unsplash.com/photo-1726066012751-2adfb5485977?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                    alt="image scammer"
-                  />
-                  <img
-                    src="https://images.unsplash.com/photo-1726066012751-2adfb5485977?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                    alt="image scammer"
-                  />
-                  <img
-                    src="https://images.unsplash.com/photo-1726066012751-2adfb5485977?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                    alt="image scammer"
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-`;
-// end MODAL DETAIL SCAMMER
-
+//Variables
 const warningHeader = document.querySelectorAll(".warning__header");
 // tạo biến trỏ tới .warning__header
-
 const endpoint = "https://67e0000c7635238f9aac34d6.mockapi.io/scammers";
 const scammerList = document.querySelector(".scammers__list");
 const today = document.querySelector(".today");
 const alertScamDesc = document.querySelector(".alert-scam__desc");
+let scammerData = [];
+//end Variables
 
 //HANDLE FORMAT DATE
 function formatDate(dateString) {
@@ -109,7 +22,7 @@ function formatDate(dateString) {
 }
 //end HANDLE FORMAT DATE
 
-// GENERATE CURRUNT DAY ON TITLE
+//GENERATE CURRUNT DAY ON TITLE
 if (today) {
   today.textContent = `Hôm nay ${formatDate()}`;
 }
@@ -155,7 +68,97 @@ function handleShowDropdown(e) {
 // scammerItems.forEach((item) => item.addEventListener("click", handleShowModal));
 
 // Hàm thêm trang modal vào đầu trang (trong body) và tự ẩn scrollbar
-function handleShowModal() {
+// Thêm id để so sánh xem modal nào để truyền dữ liệu modal đó vào chính xác
+function handleShowModal(id) {
+  const scammer = scammerData.find((item) => item.id === id);
+  const ModalHTML = `
+<section class="modal">
+       <div class="modal__overlay"></div>
+       <div class="modal__content">
+         <div class="modal__header">
+           <div class="modal__header-title">Chi tiết tố cáo</div>
+           <div class="modal__header-close">
+             <img src="./assets/images/icon/close-icon.svg" alt="close icon" />
+           </div>
+         </div>
+         <div class="modal__body">
+           <div class="modal__group">
+             <div class="modal__profile">
+               <div class="modal__profile-avatar">
+                 <img src="./assets/images/Avatars/avatar-1.png" alt="avatar-scammer" />
+               </div>
+               <div class="modal__info">
+                 <h4 class="modal__info-name">Nguyễn Văn A</h4>
+                 <p class="modal__info-dsc">#50 - Tố vào ngày 12/02/2024</p>
+               </div>
+             </div>
+             <div class="modal__detail">
+               <span class="modal__detail-title">Số điện thoại</span>
+               <span class="modal__detail-text">0333990859</span>
+             </div>
+             <div class="modal__detail">
+               <span class="modal__detail-title">Số tài khoản</span>
+               <span class="modal__detail-text">342423443</span>
+             </div>
+             <div class="modal__detail">
+               <span class="modal__detail-title">Ngân hàng</span>
+               <span class="modal__detail-text">ACB</span>
+             </div>
+           </div>
+           <div class="modal__group">
+             <div class="modal__profile">
+               <div class="modal__profile-avatar">
+                 <img src="./assets/images/Avatars/avatar-2.png" alt="avatar-scammer" />
+               </div>
+               <div class="modal__info">
+                 <h4 class="modal__info-name">Nguyễn Văn B</h4>
+                 <p class="modal__info-dsc">Người tố cáo</p>
+               </div>
+             </div>
+             <div class="modal__detail">
+               <span class="modal__detail-title">Trạng thái</span>
+               <span class="modal__detail-text">Nạn nhân</span>
+             </div>
+             <div class="modal__detail">
+               <span class="modal__detail-title">Liên hệ</span>
+               <span class="modal__detail-text">0356655665</span>
+             </div>
+             <div class="modal__textarea">
+               <span class="modal__detail-title">Nội dung tố cáo</span>
+               <p class="modal__textarea-content">
+                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente asperiores,
+                 tenetur eius a, nemo incidunt necessitatibus sunt vitae voluptates molestias
+                 aliquid explicabo dolore quia possimus fugiat eveniet veniam inventore nisi.
+               </p>
+             </div>
+             <div class="modal__images">
+               <span class="modal__detail-title">Hình ảnh liên quan</span>
+               <div class="modal__preview-images">
+                 <img
+                   src="https://images.unsplash.com/photo-1726066012751-2adfb5485977?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                   alt="image scammer"
+                 />
+                 <img
+                   src="https://images.unsplash.com/photo-1726066012751-2adfb5485977?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                   alt="image scammer"
+                 />
+                 <img
+                   src="https://images.unsplash.com/photo-1726066012751-2adfb5485977?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                   alt="image scammer"
+                 />
+                 <img
+                   src="https://images.unsplash.com/photo-1726066012751-2adfb5485977?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                   alt="image scammer"
+                 />
+               </div>
+             </div>
+           </div>
+         </div>
+       </div>
+     </section>
+`;
+
+  console.log(scammer);
   document.body.insertAdjacentHTML("afterbegin", ModalHTML);
   document.body.style.overflow = "hidden";
 }
@@ -171,7 +174,7 @@ document.body.addEventListener("click", (e) => {
     //   modal.remove();.
     //   document.body.style.overflow = "auto";
   } else if (e.target.matches(".scammers__item")) {
-    handleShowModal();
+    handleShowModal(e.target.dataset.id);
   }
 });
 
@@ -209,9 +212,9 @@ function renderScammerToday(data) {
 async function getScammer() {
   try {
     const response = await axios.get(endpoint);
-    const data = await response.data;
-    renderScammerToday(data);
-
+    scammerData = await response.data;
+    // Lấy dữ liệu database lưu vô biến scammerData để sử dụng cho hàm khác
+    renderScammerToday(scammerData);
     console.log(response.data);
   } catch (error) {
     console.error(error);
