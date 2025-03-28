0978c023dc25b8c6a3489e289dc8fc15f1e8dbb5
VyD1nk1902 <vy.dinh.2929@gmail.com>
2025-03-28 11:24:05 +0700

    Optimize fixed header
    Make the fixed header display more smoothly when scrolling up and down
    by moving height-related variables outside of the scroll event to avoid
    repeated calculations. Use a flag variable to track state and prevent
    redundent assignment of the fixed class.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,15 +1,18 @@
 // Header fixed
 const header = document.querySelector(".header");
+const headerHeight = header.offsetHeight;
+const marginTopHeader = parseFloat(getComputedStyle(header).marginTop);
+const totalHeaderHeight = headerHeight + marginTopHeader;
+let isFixed = false;
 
 window.addEventListener("scroll", () => {
-  const headerHeight = header.offsetHeight;
-  const marginTopHeader = parseFloat(getComputedStyle(header).marginTop);
-  const totalHeaderHeight = headerHeight + marginTopHeader;
-  if (window.scrollY > totalHeaderHeight) {
+  if (window.scrollY > totalHeaderHeight && !isFixed) {
     header.classList.add("fixed");
-    document.body.style.paddingTop = `${totalHeaderHeight}`;
-  } else {
+    document.body.style.paddingTop = `${totalHeaderHeight}px`;
+    isFixed = true;
+  } else if (window.scrollY <= totalHeaderHeight && isFixed) {
     header.classList.remove("fixed");
-    document.body.style.paddingTop = 0;
+    document.body.style.paddingTop = "0px";
+    isFixed = false;
   }
 });
