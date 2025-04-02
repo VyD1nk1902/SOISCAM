b0c612c9c768761e479dabbaafe96c73a6266050
VyD1nk1902 <vy.dinh.2929@gmail.com>
2025-04-02 12:35:11 +0700

    Reuse endpoint for MockAPI in global.js and rendering data
    Use Axios with try-catch to fetch data from an API,
    and create a function to render the retrieved data
    to the dashboard page.

---
+++ admin/js/pending.js
@@ -0,0 +1,51 @@
+// VARIABLES
+let scammerData = [];
+const dashboardTableBody = document.querySelector(".dashboard__table-body");
+const dashboardTopTotal = document.querySelector(".dashboard__top-total");
+const dashboardTableWrap = document.querySelector(".dashboard-table-wrap");
+// end VARIABLES
+
+// HANDLE RENDER SCAMMER PENDING
+function handleRenderScammerApprove(item) {
+  const tableBodyItemHTML = `
+    <tr class="dashboard__table-bodyItem">
+        <td>#${item.id}</td>
+        <td>${item.nameScammer}</td>
+        <td>${item.bankNumber}</td>
+        <td>${item.bankName}</td>
+        <td>${item.phoneScammer}</td>
+        <td>${item.nameSender}</td>
+        <td>${formatDate(item.date)}</td>
+        <td class="table-action">
+            <span class="table-action__icon table-action__view">
+                <i class="fa-solid fa-eye"></i>
+            </span>
+            <span class="table-action__icon table-action__remove">
+                <i class="fa-solid fa-trash"></i>
+            </span>
+        </td>
+    </tr>
+    `;
+  dashboardTableBody.insertAdjacentHTML("afterbegin", tableBodyItemHTML);
+}
+// end HANDLE RENDER SCAMMER PENDING
+
+// HANDLE GET SCAMMER PENDING
+async function handleGetScammerApprove() {
+  try {
+    const response = await axios.get(endpoint);
+    const scammerData = await response.data;
+    const pendingScammerData = scammerData.filter((item) => item.approve === false);
+    dashboardTopTotal.textContent = `(có ${pendingScammerData.length} đơn tố cáo đợi duyệt)`;
+    if (pendingScammerData?.length > 0) {
+      // (=== pendingScammerData && pendingScammerData.length)
+      pendingScammerData.forEach((item) => {
+        handleRenderScammerApprove(item);
+      });
+    } else {
+      dashboardTableWrap.insertAdjacentHTML("beforeend", renderNotFoundHTML("Chưa có đơn nào", (admin = true)));
+    }
+  } catch (error) {}
+}
+handleGetScammerApprove();
+// end HANDLE GET SCAMMER PENDING
